d5b0bc0290ac279b7373f1cf9539d80d325d0cfb
Jordan Budisantoso <jbudisantoso@wlapcs.org>
2018-01-02 07:18:01 +0000

    first commit

--- sketch.js
+++ sketch.js
@@ -1,30 +1,81 @@
-function setup(){
-    createCanvas(600,400);
-    // noStroke();
-    // textSize(32);
+var lpkidsred;
+var lpkidsgreen;
+var lpkidsblue;
+var lpkidsgray;
+var television;
+var multipleKeys = [];
+
+function preload(){
+    lpkidsgray = loadImage("images/lpkids-gray.png");
+    lpkidsred = loadImage("images/lpkids-red.png");
+    lpkidsgreen = loadImage("images/lpkids-green.png");
+    lpkidsblue = loadImage("images/lpkids-blue.png");
     
+    television = loadImage("images/old-tv.png");
+}
+
+function setup(){
+    createCanvas(749,524).parent("sketch-holder");
+}
+
+
+function keyPressed(){
+    append(multipleKeys,key);
+    console.log(multipleKeys);
+}
+
+function keyReleased(){
+    
+    multipleKeys.forEach(function(item,index,array){
+        if(item == key){
+            multipleKeys.splice(index,index+1);
+        }
+    });
 }
 
 function draw(){
-    jaylin_time = (60-second())*7;
-    countdown = 60-second();
-    countdownSize = 300;
-    textY = 280;
-    
-    if(countdown == 24 || countdown == 8){
-        countdown = 'Kobe';
-        countdownSize = 200;
-        textY = 225;
+    if(keyIsPressed){
+        multipleKeys.forEach(function(item,index,array){
+            if(item == "R"){
+                createBlend();
+                image(lpkidsred,50,50);
+            }
+            if(item == "G"){
+                createBlend();
+                image(lpkidsgreen,50,50);
+            }
+            if(item == "B"){
+                createBlend();
+                image(lpkidsblue,50,50);
+            }
+            if(item == " "){
+                createBlend();
+                image(lpkidsred,50,50);
+                image(lpkidsgreen,50,50);
+                image(lpkidsblue,50,50);
+            }
+        });
+        
+        blendMode(NORMAL);
+        televisionBorder();
     }
-    
-    background('#72008e');
-    
-    textSize(countdownSize);
-    fill('#ffaa00');
-    textAlign(CENTER);
-    text(countdown,300,textY);
-    
-    strokeWeight(5);
-    fill(100,100,100,200);
-    ellipse(300, 200, jaylin_time, jaylin_time);
+    else{
+        grayScreen();
+    }
+}
+
+function createBlend(){
+    fill(0);
+    rect(50,50,500,374);
+    blendMode(LIGHTEST);    
+}
+
+function grayScreen(){
+    blendMode(NORMAL);
+    image(lpkidsgray,50,50);
+    televisionBorder();
+}
+
+function televisionBorder(){
+    image(television,0,0);
 }
